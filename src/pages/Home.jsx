76eee06f5a4931da96006ed60134db501b5ae0bb
Petr Kekalo <kekalo.petr@gmail.com>
2025-03-31 06:30:30 -0400

    fix: enable routing with BrowserRouter and display MUI button on Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to Home Page</h1>
+      <Button variant="contained" color="primary">
+        MUI Button
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
